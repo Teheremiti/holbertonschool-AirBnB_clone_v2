b10410d7581042d621f0ddf14859f51efcdb37f2
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-08-15 13:37:06 -1000

    New route to check if a var is an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""HBNB module - New route for /number"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python/<text>", defaults={'text': "is cool"}, strict_slashes=False)
+def python(text):
+    return "Python " +  text.replace("_", " ")
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def isnumber(n):
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
